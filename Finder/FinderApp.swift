bae3bdb26733ba57eae5450c00fdb9e97019f7e3
Ilya Kalin <ilya.muromec888@gmail.com>
2025-02-06 18:14:43 +0300

    Initial Commit

---
+++ Finder/FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderApp.swift
+//  Finder
+//
+//  Created by Ilya Kalin on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
